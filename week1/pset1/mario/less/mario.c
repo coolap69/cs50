1daa286381c3167211051c319dff8599c48e69cd
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-04-05 16:30:50 +0000

    CS50 Mario less comfortable done

---
+++ week1/pset1/mario/less/mario.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    int top = 2;
+    int steps = 0;
+
+    do
+    {
+        // Enter height
+        height = get_int("Enter height (positive number between 0 and 23): ");
+    }
+    while (height < 0 || height > 23);
+
+    // Build the pyramid
+    int row = height + 1;
+    int spaces = row - top;
+
+    // Loop for rows of stairs aka height
+    for (int i = 0; i < height; i++)
+    {
+        // Print spaces
+        for (int j = 0; j < spaces; j++)
+        {
+            printf(" ");
+        }
+
+        // Create number for steps
+        steps = row - spaces;
+
+        // Decrement spaces
+        spaces--;
+
+        // Print hashes aka steps
+        for (int j = 0; j < steps; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
