82fb540dfd04af1354f5991e9bf5090ffd9476c7
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-04-09 16:01:37 +0000

    working with two names, but not three

--- pset2/initials/initials.c
+++ pset2/initials/initials.c
@@ -10,14 +10,50 @@ int main(void)
     printf("%s\n", s);
 
     // Check to see if first letter of name is lowercase or uppercase
-    if (islower(s[0]))
+    // if (islower(s[0]))
+    // {
+    //     // If lowercase, convert to uppercase and print character
+    //     s[0] = toupper(s[0]);
+    //     printf("%c\n", s[0]);
+    // }
+    // else // Print uppercase character
+    // {
+    //     printf("%c\n", s[0]);
+    // }
+
+    // Get length of string
+
+    // printf("%lu\n", strlen(s));
+
+    for (int i = 0; i < strlen(s); i++)
     {
-        // If lowercase, convert to uppercase and print character
-        s[0] = toupper(s[0]);
-        printf("%c\n", s[0]);
+        if (s[i] == s[0])
+        {
+            if (islower(s[0]))
+            {
+                s[0] = toupper(s[0]);
+                printf("%c", s[0]);
+            }
+            else
+            {
+                printf("%c", s[0]);
+            }
+        }
     }
-    else // Print uppercase character
+    for (int j = 0; j < strlen(s); j++)
     {
-        printf("%c\n", s[0]);
+        if (s[j] == ' ')
+        {
+            if (islower(s[j + 1]))
+            {
+                s[j + 1] = toupper(s[j + 1]);
+                printf("%c\n", s[j + 1]);
+            }
+            else
+            {
+                printf("%c\n", s[j + 1]);
+            }
+        }
     }
+
 }
