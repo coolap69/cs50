7a3f3863552ce563e305697f532f6ec801055f66
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-04-10 14:03:30 +0000

    CS50 Caesar's Cipher Less Completed

--- pset2/caesar/less/caesar.c
+++ pset2/caesar/less/caesar.c
@@ -11,14 +11,15 @@ int main(int argc, string argv[])
 
     // Get key
 
-    // Check to see if user entered key
+    // Check to see if user entered key and only two arguments
     if (argc == 2)
     {
+        // If two arguments entered and second is a number, convert number entered from string to integer
         key = atoi(argv[1]);
-        printf("%d\n", key);
     }
     else
     {
+        // Return error message
         printf("Error: Must enter a non negative integer after ./caesar.\n");
         return 1;
     }
@@ -26,25 +27,49 @@ int main(int argc, string argv[])
     // Get plaintext from user
     plaintext = get_string("plaintext: ");
 
-    // Output ciphertext from user
+    // Cipheded text will appear after "ciphertext: "
     printf("ciphertext: ");
 
     // Loop through plaintext adding key to each character
     for (int i = 0; i < strlen(plaintext); i++)
     {
 
-        // Preserve case
+        // Check to see if character is Alpha or not
         if (isalpha(plaintext[i]))
         {
-            if (toupper(plaintext[i]))
-            printf("%c", plaintext[i] + key);
+            // If Alpha, check to see if character is uppercase
+            if (isupper(plaintext[i]))
+            {
+                // Convert uppercase character from ASCII to Alphabetical Index
+                int alphaIndexUpper = plaintext[i] + key - 65;
+                // Convert from Alpha to Cipher
+                int cipheredUpper = alphaIndexUpper % 26 + 65;
+                //Print character
+                printf("%c", cipheredUpper);
+
+            }
+            // If Alpha and not uppercase, character is lowercase
+            else
+            {
+                // Convert lowercase character from ASCII to Alphabetical Index
+                int alphaIndexLower = plaintext[i] + key - 97;
+                // Convert from Alpha to Cipher
+                int cipheredLower = alphaIndexLower % 26 + 97;
+                // Print character
+                printf("%c", cipheredLower);
+            }
         }
+        // If not an Alpha character, no need to convert
         else
         {
-            printf("%c", plaintext[i] + key);
+            // Print character
+            printf("%c", plaintext[i]);
         }
+
     }
+    // Print new line after printing out all text
     printf("\n");
 
+    // Program ran successfully
     return 0;
 }
